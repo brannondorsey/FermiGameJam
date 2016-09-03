5b96553e947613a1433a9e5b4f0d7cb3a2c7c407
Brannon Dorsey <brannon@brannondorsey.com>
2016-09-03 17:03:26 -0500

    Add geolocation entrypoint to server.js

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -5,11 +5,39 @@ let peer = new Peer({
 });
 
 peer.on('open', id => {
-  console.log('My peer ID is: ' + id);
+  console.log('My peer ID is: ' + id)
 });
 
-let socket = io('localhost:3000');
+let socket = io('localhost:3000')
 
-if ("geolocation" in navigator) {
-	
+getPosition()
+	.then((pos) => {
+		let coords = {
+			lat: pos.coords.latitude,
+			lon: pos.coords.longitude
+		}
+		socket.emit('geolocation', coords)
+	})
+	.catch(onGeoFailed)
+
+function getPosition() {
+	return new Promise(function(res, rej){
+
+		if ('geolocation' in navigator) {
+			let pos = navigator.geolocation.getCurrentPosition(success, fail)
+			
+			function success(pos) {
+				res(pos)
+			}
+
+			function fail() {
+				rej('Error getting current geolocation position')
+			}
+
+		} else rej('Your browser doesn\'t support geolocation')
+	})
+}
+
+function onGeoFailed(err) {
+	socket.emit('geolocation', null)
 }
--- server/server.js
+++ server/server.js
@@ -11,15 +11,23 @@ peerServer.on('connection', id => {
 	// civ.star = assignStar(geo, ids)
 	// ids.set(id, civ)
 
-	console.log(`client connected with id: ${id}`)
+	console.log(`a peer client connected with id: ${id}`)
 });
 
 io.on('connection', socket =>{
 	console.log('a socket.io client connected')
-
+	socket.on('geolocation', geo => {
+		if (geo) {
+			
+		} else {
+			// geolocation was not supported by the 
+			// browser, or the user chose not share it
+			
+		}
+	})
 });
 
-io.listen(3000);
+io.listen(3000)
 
 function assignStar(geo, ids) {
 	let assignedStars = ids.values().map(civ => civ.star)
