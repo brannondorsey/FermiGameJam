0d347ce5e41c317c08f3d66010c9b1ae1d827d24
Brannon Dorsey <brannon@brannondorsey.com>
2016-09-03 15:06:16 -0500

    Add client/public stuffs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
 import {PeerServer} from 'peer'
 
-let server = PeerServer({port: 9000, path: '/fermi'});
+let server = PeerServer({port: 9000, path: '/fermi'})
+server.on('connection', id => {
+	console.log(`client connected with id: ${id}`)
+});
