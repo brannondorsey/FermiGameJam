5efd0dc5a666f00d33aa4a5923464834ff9f7f3a
McTavish McArdle <mctavishmcardle@gmail.com>
2016-09-04 16:50:35 -0500

    star catalogue ingestion, star assignment

--- server/server.js
+++ server/server.js
@@ -1,34 +1,99 @@
 import {PeerServer} from 'peer'
+let Baby = require('babyparse')
 
 let io = require('socket.io')();
 
 let ids = new Map()
 let peerServer = PeerServer({port: 9000, path: '/fermi'})
 
-peerServer.on('connection', id => {
-	
-	// let civ = new Civilization()
-	// civ.star = assignStar(geo, ids)
-	// ids.set(id, civ)
+// reads star catalogue into object
+let parsed_hygdata = Baby.parseFiles(
+    "data/hygdata_v3.csv",
+    {
+        header: true,
+        dynoamicTyping: true,
+        complete: function(results) {
+            console.log("parsed hygdata")
+        }
+    }
+)
 
-	console.log(`a peer client connected with id: ${id}`)
+// filters star catalogue
+let star_list = parsed_hygdata.data
+    .filter(star => star.proper !== 'Sol')
+    .filter(star => star.dist < 10000000)
+
+peerServer.on('connection', id => {
+
+    // let civ = new Civilization()
+    // civ.star = assignStar(geo, ids)
+    // ids.set(id, civ)
+
+    console.log(`a peer client connected with id: ${id}`)
 });
 
 io.on('connection', socket =>{
-	console.log('a socket.io client connected')
-	socket.on('geolocation', geo => {
-		if (geo) {
-			
-		} else {
-			// geolocation was not supported by the 
-			// browser, or the user chose not share it
-			
-		}
-	})
+    console.log('a socket.io client connected')
+    socket.on('geolocation', geo => {
+    	if (geo) {
+
+    	} else {
+    		// geolocation was not supported by the
+    		// browser, or the user chose not share it
+
+    	}
+    })
 });
 
 io.listen(3000)
 
-function assignStar(geo, ids) {
-	let assignedStars = ids.values().map(civ => civ.star)
+// computes central angle between two polar vectors
+//  coordinates in radians
+function centralAngle (lat1, lon1, lat2, lon2) {
+    return Math.acos(
+        Math.sin(lat1) * Math.sin(lat2) +
+        Math.cos(lat1) * Math.cos(lat2) * Math.cos(Math.abs(lon2 - lon1))
+    )
+}
+
+// converts degrees to radians
+function radians (degrees) {
+    return degrees * Math.PI / 180
+}
+
+// selects star closest to specified geolocation not in input `ids` object
+//  (values of `ids` must include `id` attribute)
+function assignStar (geo, ids) {
+    let assignedStars = Array.from(ids.values()).map(civ => civ.star)
+
+    let lat = radians(geo.lat)
+    let lon = radians(geo.lon)
+
+    let sorted_star_list = star_list
+        .filter(
+            star =>
+                assignedStars.indexOf(star.id) === -1
+        )
+        .sort(
+            (star1, star2) =>
+                {
+                    let star1_ca = centralAngle(
+                        lat,
+                        lon,
+                        star1.decrad,
+                        star1.rarad
+                    )
+
+                    let star2_ca = centralAngle(
+                        lat,
+                        lon,
+                        star2.decrad,
+                        star2.rarad
+                    )
+
+                    return star1_ca < star2_ca
+                }
+        )
+
+    return sorted_star_list[sorted_star_list.length - 1]
 }
