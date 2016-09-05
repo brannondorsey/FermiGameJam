0f390744162043af90b703603d696d30c545f3e0
Brannon Dorsey <brannon@brannondorsey.com>
2016-09-05 11:28:25 -0500

    remove test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-import {fetch} from 'node-fetch'
-
-let params = new UrlSearchParams()
-params.append('foo', 'bar')
-fetch('google.com', { body: params })
-	.then( data => console.log(data))
-	.catch(err => console.error(err))
